f2bfcdb3ec7f09ff035738e99c1603ee8cb8710b
Pyrex <gamboclifford11@gmail.com>
2023-03-20 14:22:20 -0700

    101-quote.c task commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * Description: main - returns 0 (succes) to stdout
- * Return: always 0
+ * Description: main - Entry point
+ * Return: always 0 (success)
  */
 int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
-	return (0);
+	return (1);
 }
