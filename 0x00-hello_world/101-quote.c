3720c79dddf8bbcff88e4d7d2fc8d49edd76ba81
Pyrex <gamboclifford11@gmail.com>
2023-03-20 14:13:48 -0700

    101-quote.c task commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints out 0 as success to stdout
+ *
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	return (0);
+}
