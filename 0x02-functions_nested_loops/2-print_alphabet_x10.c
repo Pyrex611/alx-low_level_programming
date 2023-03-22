60b80b0f4e0be8751eac03da0a951e0509554f4e
Pyrex <gamboclifford11@gmail.com>
2023-03-22 16:26:38 -0700

    2-print_alphabet_x10.c task commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - write alphabets from a -z x10 times
+ * Return - void
+ */
+
+void print_alphabet_x10(void)
+{
+
+	char myAlph;
+	int i = 0;
+	while (i < 10)
+	{
+	for (myAlph = 'a';  myAlph <= 'z'; myAlph++)
+	{
+		_putchar(myAlph);
+	}
+	_putchar('\n');
+	i++;
+	}
+
+
+}
