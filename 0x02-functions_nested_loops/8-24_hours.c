8d90b313ae890eab92adeaf76b5e7635c1547a80
Pyrex <gamboclifford11@gmail.com>
2023-03-22 18:51:27 -0700

    9-times_table.c task commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer, from 00:00 to 23:59.
+ * jack_bauer - prints every minute of the day of Jack Bauer
  *
  * Return: void.
  */
@@ -12,7 +12,7 @@ void jack_bauer(void)
 
 	for (a = 0; a <= 2; a++)
 	{
-		for (b = 0; b <=9; b++)
+		for (b = 0; b <= 9; b++)
 		{
 			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 			{
